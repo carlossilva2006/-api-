e9094f39c2f9b6967b9867fbc61c87485dd8b031
carlos <carlosascii@hotmail.com>
2020-05-17 22:49:24 -0400

    detalles

--- src/ejemplo.js
+++ src/ejemplo.js
@@ -1,17 +1,16 @@
-//const morgan = require('morgan');
+
 const argv = require('./config/yargs').argv;
 const persona = require('./src/form-perso');
 const colors =  require('colors');
 
 let comando = argv._[0];
-// settings se podria decir que aqui estoy creando una variable y el listen la uso
+
 app.set('port',process.env.PORT || 3000); 
-        // en caso exista puerto definido que lo tome sino tome el 3000 por defecto
 
 // middlewares 
-//app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false})); // para enter los datos que vienen desde formularios input y esas cosas
-app.use(express.json());      // para soportar formatos json, permite poder recibir y entender este formato
+
+app.use(express.urlencoded({extended: false})); 
+app.use(express.json());      
 
 // routes  
 app.get('/',(req, res) => {
@@ -21,8 +20,7 @@ app.get('/',(req, res) => {
 
 // Empezando el servidor
 app.listen(app.get('port'), () => {
-    console.log(`Servidor en puerto ${app.get('port')}`); //pone asi porque despues se puede obtener 
-                                                          //desde cualquier parte de la aplicacion
+    console.log(`Servidor en puerto ${app.get('port')}`); 
 });
 
 
